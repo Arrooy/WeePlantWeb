2a691331de885082b4920e92b1f45535a39a0a55
HobJob <adria.arroyo@outlook.es>
2019-12-11 08:28:35 +0100

    Added logo

--- client/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    let sound = document.getElementById("player");
-    sound.currentTime = 0;
-    sound.loop = true; //if you want it to restart playing automatically when it ends
-    sound.play();
-});
