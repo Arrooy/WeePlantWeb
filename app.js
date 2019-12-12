dc4a303840641a427ece420f2860f46fcb59bf32
HobJob <adria.arroyo@outlook.es>
2019-12-12 20:12:17 +0100

    Node modules added. Debug added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var SQL_HOST = ""
+var SQL_USERNAME = ""
+var SQL_PASSWORD = ""
+
 var express = require('express');
+var mysql = require('mysql');
+
 var app = express();
-//var nodemailer = require('nodemailer');
 var serv = require('http').Server(app);
 
 app.get('/', function(req, res) {
@@ -11,86 +16,24 @@ app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
+
 /*
+var con = mysql.createConnection({
+    host: SQL_HOST,
+    user: SQL_USERNAME,
+    password: SQL_PASSWORD
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+*/
 var io = require('socket.io')(serv, {});
 
 io.sockets.on('connection', function(socket) {
-    
-        socket.on("MyIpIs", function(data) {
-
-            var emailEmiter = 'actura@actura12.com';
-            var emailEmiterPass = 'Numancia56';
-
-            if (ipList.indexOf(data.ip) === -1) {
-
-
-                var mailOK = data.mail.indexOf("@") !== -1 && data.mail.indexOf(".") !== -1 && data.mail.length >= 5;
-                var allGood = mailOK && data.nom.length !== 0 && data.cog.length !== 0 && data.message.length !== 0;
-
-                if (allGood) {
-
-                    var transporter = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: emailEmiter,
-                            pass: emailEmiterPass
-                        }
-                    });
-
-                    var subject;
-                    var text;
-
-                    if (data.idioma === 1) {
-                        //Catala
-                        subject = "Formulari Actura12";
-                        text = "Hem rebut el teu formulari correctament, ens posarem en contacte el mes aviat possible.";
-                    } else if (data.idioma === 2) {
-                        //Angles
-                        subject = "Actura12 Form";
-                        text = "We have received your form correctly, we will contact you as soon as possible.";
-                    } else {
-                        //Castella
-                        subject = "Formulario Actura12";
-                        text = "Hemos recivido tu formulario correctamente, nos pondremos en contacto lo antes posible.";
-                    }
-
-                    var mailOptions = {
-                        from: emailEmiter,
-                        to: data.mail,
-                        subject: subject,
-                        html: "<p>" + text + "</p>"
-                    };
-
-                    transporter.sendMail(mailOptions, function(error, info) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            console.log('Email sent: ' + info.response);
-                        }
-                    });
-
-                    var mailOptions2 = {
-                        from: emailEmiter,
-                        to: emailEmiter,
-                        subject: "Formulari de " + data.nom + " " + data.cog,
-                        html: "<p>Nom: " + data.nom + " " + data.cog + "</p><p> Email: " + data.mail + "</p>" +
-                            "<p> Missatge: " + data.message + "</p>"
-                    };
-
-                    transporter.sendMail(mailOptions2, function(error, info) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            console.log('Email sent: ' + info.response);
-                        }
-                    });
-                }
-
-            } else {
-                console.log("IP ALREADY REGISTERED");
-            }
-
-            ipList.push(data.ip.ip);
-        })
+    socket.on("newPot", function(data) {
+        console.log("Request QR code of pot #" + data);
+    })
 });
-*/
--- client/main.js
+++ client/main.js
@@ -1,22 +1,26 @@
+var socket = io();
+
 $(document).ready(function() {
-    $("#pot1").on('click', function() {
-        console.log("HEY");
-    });
+
+    $("#plantMenu").css("display", "none");
+    $("#mainMenu").css("display", "block");
+    configIcon();
+    configPots();
+});
+
+var configIcon = function() {
     $("#icon").on('click', function() {
         $("#mainMenu").css("display", "block");
         $("#plantMenu").css("display", "none");
     });
-    $("#plantMenu").css("display", "none");
-    $("#mainMenu").css("display", "block");
-    addHovers();
-});
-
-var addHovers = function() {
     $("#icon").hover(function() {
         $(this).css("opacity", "0.75");
     }, function() {
         $(this).css("opacity", "1");
     });
+}
+var configPots = function() {
+
     for (let index = 1; index <= 3; index++) {
         let item = $("#pot" + index);
         item.hover(function() {
@@ -24,9 +28,24 @@ var addHovers = function() {
         }, function() {
             $(this).css("opacity", "1");
         });
+
         item.on('click', function() {
+            socket.emit("newPot",
+                index
+            );
             $("#mainMenu").css("display", "none");
             $("#plantMenu").css("display", "inline-block");
         });
     }
+}
+
+var draw = function() {
+    var c = document.getElementById("graphCavas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(50, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(100, 20, 100, 100);
 }
