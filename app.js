342359dc9468eda383435e01bac344218695468f
HobJob <adria.arroyo@outlook.es>
2020-02-06 18:44:34 +0100

    Added icons and stuff

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var SQL_HOST = ""
-var SQL_USERNAME = ""
-var SQL_PASSWORD = ""
+var SQL_HOST = '188.166.9.142'
+var SQL_USERNAME = 'wee'
+var SQL_PASSWORD = 'weeplant1234'
 
 var express = require('express');
-var mysql = require('mysql');
 
 var app = express();
 var serv = require('http').Server(app);
+var WebSocketServer = require('websocket').server;
+var io = require('socket.io')(serv, {});
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
@@ -17,23 +20,68 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
-/*
-var con = mysql.createConnection({
-    host: SQL_HOST,
+
+const { Client } = require('pg')
+
+const client = new Client({
     user: SQL_USERNAME,
-    password: SQL_PASSWORD
-});
+    host: SQL_HOST,
+    database: SQL_USERNAME,
+    password: SQL_PASSWORD,
+    port: 5432,
+})
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+client.connect()
 
-*/
-var io = require('socket.io')(serv, {});
+client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+})
 
 io.sockets.on('connection', function(socket) {
     socket.on("newPot", function(data) {
         console.log("Request QR code of pot #" + data);
     })
+});
+
+wsServer = new WebSocketServer({
+    httpServer: serv,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+
+    console.log((new Date()) + ' Connection accepted.');
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        } else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
 });
--- client/main.js
+++ client/main.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+var time = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+];
+var data = [
+    [0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5],
+    [5, 5, 5, 4, 4, 3, 3, 3, 3, 2, 2, 2, 1, 0],
+    [0, 1, 2, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+];
+
+var plantNames = ["Tomates", "Rosa", "Margarita"];
+var plantAges = [5, 10, 20];
+
 $(document).ready(function() {
 
     $("#plantMenu").css("display", "none");
@@ -33,14 +47,99 @@ var configPots = function() {
             socket.emit("newPot",
                 index
             );
+
+            changeData(index - 1);
             $("#mainMenu").css("display", "none");
             $("#plantMenu").css("display", "inline-block");
         });
     }
 }
 
+
+//Retorna una tupla amb les dades i els instants de temps de cada dada.
+var getGraphData = function(index) {
+    return [time[index], data[index]];
+}
+
+var getName = function(index) {
+    return plantNames[index];
+}
+
+var getAge = function(index) {
+    return plantAges[index];
+}
+
+var changeData = function(index) {
+    let data, time, name, age;
+
+    //Get all the data.
+    [time, data] = getGraphData(index);
+    name = getName(index);
+    age = getAge(index);
+    console.log(name + "" + age);
+
+    //Update the view with the new data.
+    $("#plantName").text(name);
+    $("#plantAge").text("Plant age: " + age + " days");
+    drawGraph(data, time);
+}
+var getMinMax = function getMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    let i = arr.length;
+
+    while (i--) {
+        min = arr[i] < min ? arr[i] : min;
+        max = arr[i] > max ? arr[i] : max;
+    }
+    return [min, max];
+}
+
+var drawGraph = function(data, time) {
+
+    let minData, maxData;
+    [minData, maxData] = getMinMax(data);
+    var c = document.getElementById("graphCavas");
+    drawAxis(c, time[0], time[time.length - 1], minData, maxData);
+}
+
+//fx,lx,fy,ly son first x, last x... per saber la resolucio de la grafica.
+var drawAxis = function(c, fx, lx, fy, ly) {
+    var ctx = c.getContext("2d");
+    //Escalat de les variables rebudes.
+    var width = c.width;
+    var height = c.height;
+
+    let maxY = 10;
+    let maxX = width - 10;
+    let originX = 10;
+    let originY = height - 10;
+
+    ctx.fillStyle = '#F0F000';
+    ctx.lineWidth = 2;
+    ctx.lineJoin = "round";
+
+    ctx.beginPath();
+    ctx.moveTo(originX, maxY);
+    ctx.lineTo(originX, originY);
+    ctx.lineTo(maxX, originY);
+    ctx.stroke();
+
+}
+
+var line = function(ctx, x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(100, 200);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#808000';
+    ctx.stroke();
+}
+
 var draw = function() {
     var c = document.getElementById("graphCavas");
+    var width = c.width;
+    var height = c.height;
     var ctx = c.getContext("2d");
     ctx.beginPath();
     ctx.arc(50, 50, 40, 0, 2 * Math.PI);
